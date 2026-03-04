7b1213b41dcb8a6f64801e4aaef292c3a5e52260
Ghesun <azkaf4215@gmail.com>
2026-03-04 21:58:22 +0700

    fix: vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // ⭐ INI FIX NYA
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
